881a41602f23e6695420b53554fd5474df7d9a42
nikhila1991 <karanam.nikhila@gmail.com>
2022-03-02 12:17:08 +0530

    nikhila commit

--- src/SeleniumTraining/LoginTest.java
+++ src/SeleniumTraining/LoginTest.java
@@ -31,6 +31,7 @@ public class LoginTest {
 		adminPassword.sendKeys("krish123");
 		driver.findElement(By.xpath("//span[.='Log in']")).click();
 		System.out.println("Login Successful");
+		System.out.println("Login Successful");
 		
 		//Thread.sleep(3000);
 
